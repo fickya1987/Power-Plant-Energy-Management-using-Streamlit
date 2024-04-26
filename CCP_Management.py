c6f2d1366d2726c907a57f4503e4d43014fadd8d
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2024-04-26 09:41:55 +0700

    Update CCP_Management.py

--- CCP_Management.py
+++ CCP_Management.py
@@ -5,10 +5,9 @@ import pickle
 import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Power Plant Energy Management")
-st.header(" Power Plant Energy Management")
+st.header(" Indonesia Combined Cycle Power Plant Management")
 
-st.write("""
-            The dataset contains 9568 data points collected from a **Combined Cycle Power Plant** over 6 years (2006-2011),when the power plant was set to work with full load. 
+st.write(""" 
             Features consist of hourly average:
             
             1. Ambient variables Temperature (T), 
@@ -19,10 +18,8 @@ st.write("""
             
             4. Exhaust Vacuum (V) 
             
-            To predict the net hourly electrical energy output (EP)  of the plant.
-            
-            
-            **The data is available of the UCI Machine Learning Repository**""")
+            To predict the net hourly electrical Energy Power Output (EP) of the plant.
+            """) 
 
 # Load the data
 df = pd.read_csv("Data_Regression.csv")
@@ -75,10 +72,10 @@ st.sidebar.markdown("Choose the values that will be used to make a prediction")
 
 
 def user_input_features():
-    ambient_temperature = st.sidebar.slider("Ambient Temperature: ", float(df.AT.min()), float(df.AT.max()), float(df.AT.mean()))
-    vacuum = st.sidebar.slider("Exhaust Vacuum: ", float(df.V.min()), float(df.V.max()), float(df.V.mean()))
-    ambient_pressure = st.sidebar.slider("Ambient Pressure: ", float(df.AP.min()), float(df.AP.max()), float(df.AP.mean()))
-    relative_humidity = st.sidebar.slider("Relative Humidity: ", float(df.RH.min()), float(df.RH.max()), float(df.RH.mean()))
+    ambient_temperature = st.sidebar.slider("Ambient Temperature (AT): ", float(df.AT.min()), float(df.AT.max()), float(df.AT.mean()))
+    vacuum = st.sidebar.slider("Exhaust Vacuum (EV): ", float(df.V.min()), float(df.V.max()), float(df.V.mean()))
+    ambient_pressure = st.sidebar.slider("Ambient Pressure (AP): ", float(df.AP.min()), float(df.AP.max()), float(df.AP.mean()))
+    relative_humidity = st.sidebar.slider("Relative Humidity (RH): ", float(df.RH.min()), float(df.RH.max()), float(df.RH.mean()))
 
     # Put the slider values that will be picked into a dictionary
     data = {
